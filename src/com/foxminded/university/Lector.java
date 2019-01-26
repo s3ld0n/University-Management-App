36644d2a5dfb36b84956fe4d189bca19bdcf07cd
Rubezh <ashavrn@protonmail.com>
2019-01-26 21:28:52 +0200

    removed constructor, added toString, equals, hashCode

--- src/com/foxminded/university/Lector.java
+++ src/com/foxminded/university/Lector.java
@@ -10,10 +10,6 @@ public class Lector implements Serializable {
     private String lastName;
     private List<Subject> subjects;
 
-    public Lector() {
-        super();
-    }
-
     public int getId() {
         return id;
     }
@@ -44,5 +40,51 @@ public class Lector implements Serializable {
 
     public void setSubjects(List<Subject> subjects) {
         this.subjects = subjects;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+        result = prime * result + id;
+        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+        result = prime * result + ((subjects == null) ? 0 : subjects.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Lector other = (Lector) obj;
+        if (firstName == null) {
+            if (other.firstName != null)
+                return false;
+        } else if (!firstName.equals(other.firstName))
+            return false;
+        if (id != other.id)
+            return false;
+        if (lastName == null) {
+            if (other.lastName != null)
+                return false;
+        } else if (!lastName.equals(other.lastName))
+            return false;
+        if (subjects == null) {
+            if (other.subjects != null)
+                return false;
+        } else if (!subjects.equals(other.subjects))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Lector [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", subjects=" + subjects
+                + "]";
     }
 }
