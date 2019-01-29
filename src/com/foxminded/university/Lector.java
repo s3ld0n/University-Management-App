f43e6362ec901699ff8801a84cbe3273fc7c01ff
Rubezh <ashavrn@protonmail.com>
2019-01-29 10:54:19 +0200

    removed all but id from hashCode and equals, removed subjects from toString

--- src/com/foxminded/university/Lector.java
+++ src/com/foxminded/university/Lector.java
@@ -57,10 +57,7 @@ public class Lector implements Serializable {
     public int hashCode() {
         final int prime = 31;
         int result = 1;
-        result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
         result = prime * result + id;
-        result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
-        result = prime * result + ((subjects == null) ? 0 : subjects.hashCode());
         return result;
     }
 
@@ -73,29 +70,13 @@ public class Lector implements Serializable {
         if (getClass() != obj.getClass())
             return false;
         Lector other = (Lector) obj;
-        if (firstName == null) {
-            if (other.firstName != null)
-                return false;
-        } else if (!firstName.equals(other.firstName))
-            return false;
         if (id != other.id)
-            return false;
-        if (lastName == null) {
-            if (other.lastName != null)
-                return false;
-        } else if (!lastName.equals(other.lastName))
-            return false;
-        if (subjects == null) {
-            if (other.subjects != null)
-                return false;
-        } else if (!subjects.equals(other.subjects))
             return false;
         return true;
     }
 
     @Override
     public String toString() {
-        return "Lector [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", subjects=" + subjects
-                + "]";
+        return "Lector [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + "]";
     }
 }
