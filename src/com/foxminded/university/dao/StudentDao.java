b1cdb7c7f49bc2623f11741cc1726a75345207f2
Rubezh <ashavrn@protonmail.com>
2019-01-31 18:44:33 +0200

    remade getAllStudents - no more redundant connections; minor style changes

--- src/com/foxminded/university/dao/StudentDao.java
+++ src/com/foxminded/university/dao/StudentDao.java
@@ -5,18 +5,18 @@ import java.util.*;
 import com.foxminded.university.domain.*;
 
 public class StudentDao extends ConnectorDao {
-    
+
     public void addStudent(Student student) {
-        
+
         Connection connection = null;
         PreparedStatement statement = null;
-        
+
         String addStudent = "INSERT INTO students (id, first_name, last_name, group_id) "
                 + "VALUES(?, ?, ?, (SELECT id FROM groups WHERE name=?));";
-        
+
         try {
-            connection = DriverManager.getConnection(url, user,password);
-            
+            connection = DriverManager.getConnection(url, user, password);
+
             statement = connection.prepareStatement(addStudent);
 
             statement.setInt(1, student.getId());
@@ -27,8 +27,7 @@ public class StudentDao extends ConnectorDao {
 
         } catch (SQLException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             try {
                 if (statement != null) {
                     statement.close();
@@ -42,32 +41,32 @@ public class StudentDao extends ConnectorDao {
             }
         }
     }
-    
+
     public Student getStudentById(int id) {
-        
+
         Connection connection = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
         Student student = null;
-        
+
         String getStudent = "SELECT students.id, first_name, last_name, name AS group_name "
                 + "FROM students JOIN groups ON students.group_id = groups.id "
-                + "WHERE students.id=?;";
-        
+                    + "WHERE students.id=?;";
+
         try {
-            connection = DriverManager.getConnection(url, user,password);            
+            connection = DriverManager.getConnection(url, user, password);
             statement = connection.prepareStatement(getStudent);
             statement.setInt(1, id);
-            
+
             resultSet = statement.executeQuery();
             resultSet.next();
 
-            String firstName = resultSet.getString("first_name"); 
+            String firstName = resultSet.getString("first_name");
             String lastName = resultSet.getString("last_name");
             String group = resultSet.getString("group_name");
-            
+
             student = new Student(id, firstName, lastName, group);
-            
+
         } catch (SQLException ex) {
             ex.printStackTrace();
         } finally {
@@ -79,21 +78,21 @@ public class StudentDao extends ConnectorDao {
                 e.printStackTrace();
             }
         }
-        
+
         return student;
     }
-    
+
     public void updateStudent(int id, String firstName, String lastName, String group) {
-        
+
         Connection connection = null;
         PreparedStatement statement = null;
 
         String updateStudent = "UPDATE students "
                 + "SET first_name = ?, last_name = ?, group_id = (SELECT id FROM groups WHERE name=?) "
                 + "WHERE id = ?";
-   
+
         try {
-            connection = DriverManager.getConnection(url, user,password);            
+            connection = DriverManager.getConnection(url, user, password);
             statement = connection.prepareStatement(updateStudent);
 
             statement.setString(1, firstName);
@@ -104,8 +103,7 @@ public class StudentDao extends ConnectorDao {
 
         } catch (SQLException e) {
             e.printStackTrace();
-        }
-        finally {
+        } finally {
             try {
                 if (statement != null) {
                     statement.close();
@@ -118,45 +116,34 @@ public class StudentDao extends ConnectorDao {
             }
         }
     }
-    
-    public List<Student> getAllStudents() {
-        List<Student> students = new ArrayList<>();
-        String studentsIds = getAllStudentsIds();
-        List<String> buffer = Arrays.asList(studentsIds.split(","));
-        
-        for (String idAsString : buffer) {
-            int id = Integer.parseInt(idAsString);
-            students.add(getStudentById(id));
-        }
-        
-        return students;
-    }
-    
-    private String getAllStudentsIds() {
 
+    public List<Student> getAllStudents() {
         Connection connection = null;
         PreparedStatement statement = null;
         ResultSet resultSet = null;
-        String rowOfIds = "";
-        
-        String getAllStudents = "SELECT string_agg(CAST(students.id AS VARCHAR(50)), ',') "
-                + "AS all_students_ids from students;";
+        List<Student> students = new ArrayList<>();
+
+        String getStudent = "SELECT students.id AS id, first_name, last_name, name AS group_name " + "FROM students "
+                + "JOIN groups ON students.group_id = groups.id;";
 
         try {
-            connection = DriverManager.getConnection(url, user,password);            
-            statement = connection.prepareStatement(getAllStudents);
+            connection = DriverManager.getConnection(url, user, password);
+            statement = connection.prepareStatement(getStudent);
+
             resultSet = statement.executeQuery();
-            resultSet.next();
-            rowOfIds = resultSet.getString("all_students_ids");
-            
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        finally {
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                String group = resultSet.getString("group_name");
+
+                students.add(new Student(id, firstName, lastName, group));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } finally {
             try {
-                if (resultSet != null) {
-                    resultSet.close();
-                }
                 if (statement != null) {
                     statement.close();
                 }
@@ -168,6 +155,6 @@ public class StudentDao extends ConnectorDao {
             }
         }
 
-        return rowOfIds;
+        return students;
     }
 }
