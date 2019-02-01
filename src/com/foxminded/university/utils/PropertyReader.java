654f4d8409c7b24628541e3e12bf0278f7fd5564
Rubezh <ashavrn@protonmail.com>
2019-02-01 20:14:35 +0200

    removed import

--- src/com/foxminded/university/utils/PropertyReader.java
+++ src/com/foxminded/university/utils/PropertyReader.java
@@ -1,8 +1,6 @@
 package com.foxminded.university.utils;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.util.Properties;
 
 public class PropertyReader {
