6f14c484bb86f41819258871b7087bd53874a8c9
Rubezh <ashavrn@protonmail.com>
2019-02-01 15:33:34 +0200

    made ordinary class, properties made private, added getConnection method

--- src/com/foxminded/university/dao/ConnectorDao.java
+++ src/com/foxminded/university/dao/ConnectorDao.java
@@ -1,18 +1,32 @@
 package com.foxminded.university.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 import com.foxminded.university.utils.PropertyReader;
 
-public abstract class ConnectorDao {
+public class ConnectorDao {
 
-    String url;
-    String user;
-    String password;
+    private String url;
+    private String user;
+    private String password;
     
     public ConnectorDao() {
         Properties properties = PropertyReader.getPropertiesFromFile("/config.properties");
         this.url = properties.getProperty("url");
         this.user = properties.getProperty("user");
         this.password = properties.getProperty("password");
+    }
+    
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return connection;
     }
 }
