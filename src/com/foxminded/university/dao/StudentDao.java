49441e1a9a18f99c35c7fb3b2b107fd96e8ce1ae
Rubezh <ashavrn@protonmail.com>
2019-02-01 13:46:20 +0200

    replaced finaly with try-with-resources in all methods

--- src/com/foxminded/university/dao/StudentDao.java
+++ src/com/foxminded/university/dao/StudentDao.java
@@ -27,21 +27,16 @@ public class StudentDao extends ConnectorDao {
 
     public Student findById(int id) throws DaoException {
 
-        Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
         Student student = null;
 
         String getStudent = "SELECT students.id, first_name, last_name, name AS group_name "
-                + "FROM students JOIN groups ON students.group_id = groups.id "
-                    + "WHERE students.id=?;";
+                + "FROM students JOIN groups ON students.group_id = groups.id " + "WHERE students.id=?;";
 
-        try {
-            connection = DriverManager.getConnection(url, user, password);
-            statement = connection.prepareStatement(getStudent);
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+                PreparedStatement statement = connection.prepareStatement(getStudent);
+                ResultSet resultSet = statement.executeQuery()) {
+            
             statement.setInt(1, id);
-
-            resultSet = statement.executeQuery();
             resultSet.next();
 
             String firstName = resultSet.getString("first_name");
@@ -52,38 +47,19 @@ public class StudentDao extends ConnectorDao {
 
         } catch (SQLException ex) {
             ex.printStackTrace();
-        } finally {
-            try {
-                if (resultSet != null) {
-                    resultSet.close();                    
-                }
-                if (statement != null) {
-                    statement.close();
-                }
-                if (connection != null) {
-                    connection.close();
-                }
-
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
 
         return student;
     }
 
     public void updateStudent(int id, String firstName, String lastName, String group) throws DaoException {
-
-        Connection connection = null;
-        PreparedStatement statement = null;
-
+        
         String updateStudent = "UPDATE students "
                 + "SET first_name = ?, last_name = ?, group_id = (SELECT id FROM groups WHERE name=?) "
                 + "WHERE id = ?";
 
-        try {
-            connection = DriverManager.getConnection(url, user, password);
-            statement = connection.prepareStatement(updateStudent);
+        try (Connection connection = DriverManager.getConnection(url, user, password);
+                PreparedStatement statement = connection.prepareStatement(updateStudent)){
 
             statement.setString(1, firstName);
             statement.setString(2, lastName);
@@ -93,18 +69,8 @@ public class StudentDao extends ConnectorDao {
 
         } catch (SQLException e) {
             e.printStackTrace();
-        } finally {
-            try {
-                if (statement != null) {
-                    statement.close();
-                }
-                if (connection != null) {
-                    connection.close();
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        }    
+        
     }
 
     public List<Student> getAllStudents() throws DaoException {
