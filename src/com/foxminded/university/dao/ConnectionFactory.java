b4ed9b5f37786e386106fc3668c99c9ccad1bf3d
Rubezh <ashavrn@protonmail.com>
2019-02-03 11:10:28 +0200

    ConnectorDao renamed -> ConnectionFactory; receiveConnection -> getConnection

--- src/com/foxminded/university/dao/ConnectionFactory.java
+++ src/com/foxminded/university/dao/ConnectionFactory.java
@@ -6,20 +6,20 @@ import java.sql.SQLException;
 import java.util.Properties;
 import com.foxminded.university.utils.PropertyReader;
 
-public class ConnectorDao {
+public class ConnectionFactory {
 
     private String url;
     private String user;
     private String password;
     
-    public ConnectorDao() {
+    public ConnectionFactory() {
         Properties properties = PropertyReader.readPropertiesFromFile("/config.properties");
         this.url = properties.getProperty("url");
         this.user = properties.getProperty("user");
         this.password = properties.getProperty("password");
     }
     
-    public Connection receiveConnection() {
+    public Connection getConnection() {
         Connection connection = null;
         try {
             connection = DriverManager.getConnection(url, user, password);
