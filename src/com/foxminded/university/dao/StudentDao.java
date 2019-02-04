13567f669aa7412794af554f8aedf7f1dcb754c4
Rubezh <ashavrn@protonmail.com>
2019-02-04 19:42:22 +0200

    ResultSet placed into try-with-resources statment; added deleteById method

--- src/com/foxminded/university/dao/StudentDao.java
+++ src/com/foxminded/university/dao/StudentDao.java
@@ -3,7 +3,6 @@ package com.foxminded.university.dao;
 import java.sql.*;
 import java.util.*;
 import com.foxminded.university.domain.*;
-import com.foxminded.university.utils.PersonalInfo;
 
 public class StudentDao {
 
@@ -12,68 +11,48 @@ public class StudentDao {
         String sql = "INSERT INTO students (id, first_name, last_name, group_id) "
                 + "VALUES(DEFAULT, ?, ?, (SELECT id FROM groups WHERE name=?)) RETURNING id;";
         
-        ResultSet resultSet = null;
-        int idOfCreatedStudent = 0;
-        
         String firstName = student.getFirstName();
         String lastName = student.getLastName();
         String group = student.getGroup();
         
         try (Connection connection = ConnectionFactory.getConnection();
              PreparedStatement statement = connection.prepareStatement(sql)){
-
+            
             statement.setString(1, firstName);
             statement.setString(2, lastName);
             statement.setString(3, group);
             
-            resultSet = statement.executeQuery();
-            resultSet.next();
-            
-            idOfCreatedStudent = resultSet.getInt(1);
+            try(ResultSet resultSet = statement.executeQuery();) {
+                resultSet.next();
+                resultSet.getInt(1);
+            }
             
             } catch (SQLException e) {
                 e.printStackTrace();
-            } finally {
-                try {
-                    resultSet.close();  
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-            }
+            } 
         
-        return new Student(new PersonalInfo(idOfCreatedStudent, firstName, lastName), group);
+        return student;
     }
     
     public Student findById(int id) {
 
         Student student = null;
-        ResultSet resultSet = null;
-        
+
         String sql = "SELECT students.id, first_name, last_name, groups.name AS group_name "
                 + "FROM students JOIN groups ON students.group_id = groups.id "
                 + "WHERE students.id=?;";
 
         try (Connection connection = ConnectionFactory.getConnection();
                 PreparedStatement statement = connection.prepareStatement(sql)){
-
             statement.setInt(1, id);
-            resultSet = statement.executeQuery();
-            resultSet.next();
 
-            String firstName = resultSet.getString("first_name");
-            String lastName = resultSet.getString("last_name");
-            String group = resultSet.getString("group_name");
-
-            student = new Student(new PersonalInfo(id, firstName, lastName), group);
-            
+            try (ResultSet resultSet = statement.executeQuery()){
+                resultSet.next();
+                student = new Student(id, resultSet.getString("first_name"),
+                        resultSet.getString("last_name"), resultSet.getString("group_name"));
+            } 
         } catch (SQLException ex) {
             ex.printStackTrace();
-        } finally {
-            try {
-                resultSet.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
         }
 
         return student;
@@ -119,7 +98,7 @@ public class StudentDao {
                 String lastName = resultSet.getString("last_name");
                 String group = resultSet.getString("group_name");
 
-                students.add(new Student(new PersonalInfo(id, firstName, lastName), group));
+                students.add(new Student(id, firstName, lastName, group));
             }
             
         } catch (SQLException ex) {
@@ -127,5 +106,24 @@ public class StudentDao {
         }
         
         return students;
+    }
+    
+    public Student delete(Student student) {
+        String sql = "DELETE FROM students WHERE id=?";
+        
+        try (Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql);){
+            
+            statement.setInt(1, student.getId());
+            
+            try (ResultSet resultSet = statement.executeQuery();){
+                
+            }
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        } 
+    
+        return student;
     }
 }
