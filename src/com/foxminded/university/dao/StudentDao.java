188c642ccd94b621757e28ea2aa8346f519bcb15
Rubezh <ashavrn@protonmail.com>
2019-02-05 15:51:24 +0200

    minor style fixes

--- src/com/foxminded/university/dao/StudentDao.java
+++ src/com/foxminded/university/dao/StudentDao.java
@@ -10,28 +10,28 @@ public class StudentDao {
 
         String sql = "INSERT INTO students (first_name, last_name, group_id) "
                 + "VALUES(?, ?, (SELECT id FROM groups WHERE name=?));";
-        
+
         try (Connection connection = ConnectionFactory.getConnection();
-             PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)){
-            
+                PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+
             statement.setString(1, student.getFirstName());
             statement.setString(2, student.getLastName());
             statement.setString(3, student.getGroup());
-            
+
             statement.executeUpdate();
-            
-            try(ResultSet resultSet = statement.getGeneratedKeys()) {
+
+            try (ResultSet resultSet = statement.getGeneratedKeys()) {
                 resultSet.next();
                 student.setId(resultSet.getInt(1));
             }
-            
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } 
-        
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         return student;
     }
-    
+
     public Student findById(int id) {
 
         Student student = null;
@@ -41,14 +41,14 @@ public class StudentDao {
                 + "WHERE students.id=?;";
 
         try (Connection connection = ConnectionFactory.getConnection();
-                PreparedStatement statement = connection.prepareStatement(sql)){
+                PreparedStatement statement = connection.prepareStatement(sql)) {
             statement.setInt(1, id);
 
-            try (ResultSet resultSet = statement.executeQuery()){
+            try (ResultSet resultSet = statement.executeQuery()) {
                 resultSet.next();
-                student = new Student(id, resultSet.getString("first_name"),
-                        resultSet.getString("last_name"), resultSet.getString("group_name"));
-            } 
+                student = new Student(id, resultSet.getString("first_name"), resultSet.getString("last_name"),
+                        resultSet.getString("group_name"));
+            }
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
@@ -57,13 +57,13 @@ public class StudentDao {
     }
 
     public Student update(Student student) {
-        
+
         String sql = "UPDATE students "
                 + "SET first_name = ?, last_name = ?, group_id = (SELECT id FROM groups WHERE name=?) "
                 + "WHERE id = ?";
 
         try (Connection connection = ConnectionFactory.getConnection();
-                PreparedStatement statement = connection.prepareStatement(sql)){
+                PreparedStatement statement = connection.prepareStatement(sql)) {
 
             statement.setString(1, student.getFirstName());
             statement.setString(2, student.getLastName());
@@ -74,14 +74,14 @@ public class StudentDao {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        
+
         return student;
     }
 
     public List<Student> findAll() {
-        
+
         List<Student> students = new ArrayList<>();
-        
+
         String sql = "SELECT students.id AS id, first_name, last_name, groups.name AS group_name "
                 + "FROM students "
                 + "JOIN groups ON students.group_id = groups.id;";
@@ -98,29 +98,29 @@ public class StudentDao {
 
                 students.add(new Student(id, firstName, lastName, group));
             }
-            
+
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        
+
         return students;
     }
-    
+
     public void deleteById(int id) {
         String sql = "DELETE FROM students WHERE id=?";
-        
+
         try (Connection connection = ConnectionFactory.getConnection();
-                PreparedStatement statement = connection.prepareStatement(sql)){
-            
+                PreparedStatement statement = connection.prepareStatement(sql)) {
+
             statement.setInt(1, id);
             int affectedRows = statement.executeUpdate();
-            
+
             if (affectedRows == 0) {
                 throw new SQLException("Deleting failed. No such id: " + id);
             }
-            
+
         } catch (SQLException ex) {
             ex.printStackTrace();
-        } 
+        }
     }
 }
