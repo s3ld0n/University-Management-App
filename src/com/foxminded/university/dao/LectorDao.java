e5a45e2bef4ca6f42655e0eaf13755a1098ce93b
Rubezh <ashavrn@protonmail.com>
2019-02-06 19:09:52 +0200

    added findById(), findAll(), appointToSubjectById(), discardSubjectById() methods and query constants for them

--- src/com/foxminded/university/dao/LectorDao.java
+++ src/com/foxminded/university/dao/LectorDao.java
@@ -1,12 +1,25 @@
 package com.foxminded.university.dao;
 
 import java.sql.*;
-import com.foxminded.university.domain.Lector;
+import java.util.*;
+import com.foxminded.university.domain.*;
 
 public class LectorDao {
     
     private static final String CREATE_QUERY = "INSERT INTO lectors (first_name, last_name) "
             + "VALUES (?, ?);";
+    
+    private static final String READ_QUERY = "SELECT id, first_name, last_name "
+            + "FROM lectors "
+            + "WHERE id = ?";
+    
+    private static final String APPOINT_TO_SUBJECT_BY_ID_QUERY = "INSERT INTO lectors_subjects (lector_id, subject_id) "
+            + "VALUES(?, ?);";
+    
+    private static final String READ_ALL_QUERY = "SELECT id, first_name, last_name FROM lectors;";
+    
+    private static final String DISCARD_SUBJECT_BY_ID_QUERY = "DELETE FROM lectors_subjects "
+            + "WHERE lector_id = ? AND subject_id = ?";
     
     public Lector create(Lector lector) {
 
@@ -29,4 +42,80 @@ public class LectorDao {
 
         return lector; 
     }
+    
+    public Lector findById(int id) {
+
+        Lector lector = null;
+
+        try (Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement(READ_QUERY)) {
+            statement.setInt(1, id);
+
+            try (ResultSet resultSet = statement.executeQuery()) {
+                resultSet.next();
+                lector = new Lector(id, resultSet.getString("first_name"), resultSet.getString("last_name"));
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return lector;
+    }
+    
+    public List<Lector> findAll() {
+
+        List<Lector> lectors = new ArrayList<>();
+        SubjectDao subjectDao = new SubjectDao();
+        
+        try (Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement(READ_ALL_QUERY);
+                ResultSet resultSet = statement.executeQuery()) {
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                
+                Lector lector = new Lector(id, firstName, lastName);
+                lector.setSubjects(subjectDao.findAllByLectorId(id));
+                
+                lectors.add(lector);
+            }
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+
+        return lectors;
+    }
+    
+    public void appointToSubjectById(Lector lector, int subjectId) {
+        
+        try (Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement(APPOINT_TO_SUBJECT_BY_ID_QUERY)) {
+            
+                statement.setInt(1, lector.getId());
+                statement.setInt(2, subjectId);
+                statement.executeUpdate();
+            
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void discardSubjectById(Lector lector, int subjectId) {
+        
+        try (Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement(DISCARD_SUBJECT_BY_ID_QUERY)) {
+            
+            statement.setInt(1, lector.getId());
+            statement.setInt(2, subjectId);
+            statement.executeUpdate();
+
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    
 }
