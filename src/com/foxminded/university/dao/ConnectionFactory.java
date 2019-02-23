e774a20e36c24c098eafb7cb77b9c444d3f72a83
Rubezh <ashavrn@protonmail.com>
2019-02-23 21:16:28 +0200

    added log4j2 libs, config file, added logging to ConnectionFactory

--- src/com/foxminded/university/dao/ConnectionFactory.java
+++ src/com/foxminded/university/dao/ConnectionFactory.java
@@ -4,20 +4,31 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.foxminded.university.utils.PropertyReader;
 
 public class ConnectionFactory {
 
     private static Properties properties = PropertyReader.readPropertiesFromFile("resources/config.properties");
+    private static Logger log = LogManager.getLogger(ConnectionFactory.class.getName());
     
     public static Connection getConnection() {
         Connection connection = null;
+        
+        log.debug("Creating a new connection...");
+
         try {
             connection = DriverManager.getConnection(properties.getProperty("url") , properties.getProperty("user"),
                     properties.getProperty("password"));
         } catch (SQLException e) {
-            e.printStackTrace();
+            log.error("Connection has not been created." , e);
+            throw new DaoException("Connection has not been created." , e);
         }
+        
+        log.debug("Connection was successfully created.");
         
         return connection;
     }
