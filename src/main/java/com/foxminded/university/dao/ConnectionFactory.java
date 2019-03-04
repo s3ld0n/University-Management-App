52c398b1e4a39f15a84925b6c42774c951b92ff6
Rubezh <ashavrn@protonmail.com>
2019-03-04 12:43:06 +0200

    substituted Property with ResourceBundle

--- src/main/java/com/foxminded/university/dao/ConnectionFactory.java
+++ src/main/java/com/foxminded/university/dao/ConnectionFactory.java
@@ -3,16 +3,14 @@ package com.foxminded.university.dao;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
+import java.util.ResourceBundle;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.foxminded.university.utils.PropertyReader;
-
 public class ConnectionFactory {
 
-    private static Properties properties = PropertyReader.readPropertiesFromFile("src/main/resources/config.properties");
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle("config");
     private static final Logger log = LogManager.getLogger(ConnectionFactory.class.getName());
     
     public static Connection getConnection() {
@@ -21,8 +19,8 @@ public class ConnectionFactory {
         log.debug("Creating a new connection");
 
         try {
-            connection = DriverManager.getConnection(properties.getProperty("url") , properties.getProperty("user"),
-                    properties.getProperty("password"));
+            connection = DriverManager.getConnection(resourceBundle.getString("url") , resourceBundle.getString("user"),
+                    resourceBundle.getString("password"));
         } catch (SQLException e) {
             log.error("Connection has not been created." , e);
             throw new DaoException("Connection has not been created." , e);
