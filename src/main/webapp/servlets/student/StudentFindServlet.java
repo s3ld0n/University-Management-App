753fc64797e781b0c0cc78f751fc76fe17cb7718
Rubezh <ashavrn@protonmail.com>
2019-03-16 16:12:01 +0200

    added groups list for dropdown

--- src/main/webapp/servlets/student/StudentFindServlet.java
+++ src/main/webapp/servlets/student/StudentFindServlet.java
@@ -1,13 +1,17 @@
 package servlets.student;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.foxminded.university.dao.crud_dao_implementations.GroupDao;
 import com.foxminded.university.dao.crud_dao_implementations.StudentDao;
+import com.foxminded.university.domain.Group;
 import com.foxminded.university.domain.Student;
 
 @WebServlet("/student-find")
@@ -21,7 +25,9 @@ public class StudentFindServlet extends HttpServlet {
         int id = Integer.parseInt(request.getParameter("id"));
         
         Student student = new StudentDao().findById(id);
-        
+        List<Group> groups = new GroupDao().findAll();
+
+        request.setAttribute("groups", groups);
         request.setAttribute("student", student);
         request.getRequestDispatcher("jsp/student/student.jsp").forward(request, response);
     }
